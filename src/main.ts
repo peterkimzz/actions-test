e843d22dbd7305055e0bc5cb0ae8479630deb581
Peter Kim <peterkimzz69@gmail.com>
2022-06-27 14:18:41 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function run() {
     const context = github.context;
 
     const sha = core.getInput("sha") || context.sha;
-    const tagName = core.getInput("tag") || dayjs().format("YY.MM.DD.sss");
+    const tagName = core.getInput("tag") || dayjs().format("YY.MMDD.ss");
     const message = core.getInput("message");
 
     const tagRequest = await octokit.rest.git.createTag({
